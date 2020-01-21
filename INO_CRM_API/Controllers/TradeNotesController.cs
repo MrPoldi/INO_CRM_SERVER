8f40f6f7d40fd077cd44a98dd81b834b9823d967
Kamil Guttmann <kamgut@st.amu.edu.pl>
2020-01-21 21:12:31 +0100

    TradeNoteController logic

--- INO_CRM_API/Controllers/TradeNotesController.cs
+++ INO_CRM_API/Controllers/TradeNotesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using INO_CRM_API.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace INO_CRM_API.Controllers
 {
@@ -25,6 +26,13 @@ namespace INO_CRM_API.Controllers
         public async Task<ActionResult<IEnumerable<TradeNoteModel>>> GetTradeNotes()
         {
             return await _context.TradeNotes.ToListAsync();
+        }
+
+        // GET: api/TradeNotes/Company/5
+        [HttpGet("Company/{id}")]
+        public async Task<ActionResult<IEnumerable<TradeNoteModel>>> GetTradeNotesForCompany(int id)
+        {
+            return await _context.TradeNotes.Where(n => n.CompanyId == id).ToListAsync();
         }
 
         // GET: api/TradeNotes/5
@@ -52,6 +60,10 @@ namespace INO_CRM_API.Controllers
                 return BadRequest();
             }
 
+            tradeNoteModel.CompanyId = _context.Companies.Where(c => c.Name == tradeNoteModel.Company.Name).Single().CompanyId;
+            tradeNoteModel.Company = null;
+            tradeNoteModel.UserId = _context.TradeNotes.AsNoTracking().Where(n => n.NoteId == tradeNoteModel.NoteId).Single().UserId;
+
             _context.Entry(tradeNoteModel).State = EntityState.Modified;
 
             try
@@ -76,9 +88,16 @@ namespace INO_CRM_API.Controllers
         // POST: api/TradeNotes
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
+        [Authorize(Roles = "Admin,Moderator")]
         [HttpPost]
         public async Task<ActionResult<TradeNoteModel>> PostTradeNoteModel(TradeNoteModel tradeNoteModel)
         {
+            tradeNoteModel.IsDeleted = false;
+            tradeNoteModel.UserId = _context.Users.Where(u => u.Login == tradeNoteModel.User.Login).Single().UserId;
+            //tradeNoteModel.CompanyId = _context.Companies.Where(c => c.Name == tradeNoteModel.Company.Name).Single().CompanyId;
+            tradeNoteModel.User = null;
+            //tradeNoteModel.Company = null;
+
             _context.TradeNotes.Add(tradeNoteModel);
             await _context.SaveChangesAsync();
 
@@ -86,6 +105,7 @@ namespace INO_CRM_API.Controllers
         }
 
         // DELETE: api/TradeNotes/5
+        [Authorize(Roles = "Admin")]
         [HttpDelete("{id}")]
         public async Task<ActionResult<TradeNoteModel>> DeleteTradeNoteModel(int id)
         {
@@ -95,7 +115,7 @@ namespace INO_CRM_API.Controllers
                 return NotFound();
             }
 
-            _context.TradeNotes.Remove(tradeNoteModel);
+            _context.TradeNotes.FindAsync(id).Result.IsDeleted = true;
             await _context.SaveChangesAsync();
 
             return tradeNoteModel;
